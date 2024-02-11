dd2f644e7d07a267ee2793e791eedd0e5e9b83c6
Öznur Demir <dmroznurr@gmail.com>
2024-02-11 12:22:36 +0300

    Completed Splash Screen

---
+++ app/src/main/java/com/example/aivoicechanger/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.example.aivoicechanger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        progressBar = findViewById(R.id.progressBar)
+        val handler = Handler()
+
+        val runnable = object : Runnable {
+            var progressStatus = 0
+
+            override fun run() {
+                progressStatus += 10
+                progressBar.progress = progressStatus
+
+                if (progressStatus >= 100) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                } else {
+                    // Yarım saniyede bir güncelleme yapmak için 500 ms bekletiyoruz
+                    handler.postDelayed(this, 500)
+                }
+            }
+        }
+
+        // İlk çağrıyı başlatmak için postDelayed kullanıyoruz
+        handler.postDelayed(runnable, 0)
+    }
+}
