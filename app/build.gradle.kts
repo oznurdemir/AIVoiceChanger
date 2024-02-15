a226b64327ed80d3b6bd27ee20a889ad56d211a6
Öznur Demir <dmroznurr@gmail.com>
2024-02-15 11:10:25 +0300

    Update Onboarding Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.aivoicechanger"
     compileSdk = 34
 
+    buildFeatures{
+      viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.aivoicechanger"
         minSdk = 24
--- app/src/main/java/com/example/aivoicechanger/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/aivoicechanger/ui/onboarding/OnboardingFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.aivoicechanger.R
+import com.example.aivoicechanger.databinding.FragmentOnboardingBinding
+
 class OnboardingFragment : Fragment() {
+    private lateinit var binding: FragmentOnboardingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        binding.buttonContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_onboardingFragment_to_homePageFragment)
+        }
+        return binding.root
     }
 }
