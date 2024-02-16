23876d3a44b0ecc4b03d81dc52dc86c71ac55448
Öznur Demir <dmroznurr@gmail.com>
2024-02-16 17:00:13 +0300

    Created DataSource and Repository Classes

---
+++ app/src/main/java/com/example/aivoicechanger/data/datasource/DataSource.kt
@@ -0,0 +1,4 @@
+package com.example.aivoicechanger.data.datasource
+
+class DataSource (){
+}
---
+++ app/src/main/java/com/example/aivoicechanger/repository/Repository.kt
@@ -0,0 +1,6 @@
+package com.example.aivoicechanger.repository
+
+import com.example.aivoicechanger.data.datasource.DataSource
+
+class Repository (dataSource: DataSource) {
+}
