79efd5916d2eb72f5d1f5b104474ac85986f0331
Öznur Demir <dmroznurr@gmail.com>
2024-02-28 19:35:25 +0300

    Created Song Data Class.

---
+++ app/src/main/java/com/example/aivoicechanger/data/entity/song/Song.kt
@@ -0,0 +1,14 @@
+package com.example.aivoicechanger.data.entity.song
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity("Song")
+data class Song (
+    @PrimaryKey(autoGenerate = true)
+    private val id : Int,
+    private val celebrityImage : Int,
+    private val celebrityName : Int,
+    private val text : String
+)
