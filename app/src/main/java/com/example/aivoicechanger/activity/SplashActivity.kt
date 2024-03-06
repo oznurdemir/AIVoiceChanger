22ab355d6c7944959994662f6b37296841415f37
Öznur Demir <dmroznurr@gmail.com>
2024-03-06 16:52:31 +0300

    Translated Splash and Main Activities to Activity Package.

--- app/src/main/java/com/example/aivoicechanger/activity/SplashActivity.kt
+++ app/src/main/java/com/example/aivoicechanger/activity/SplashActivity.kt
@@ -1,10 +1,11 @@
-package com.example.aivoicechanger
+package com.example.aivoicechanger.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ProgressBar
+import com.example.aivoicechanger.R
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
